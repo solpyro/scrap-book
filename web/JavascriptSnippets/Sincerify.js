fb8b8c1db64bbfee8e6fe1b894dbc692dc1e21d7
Stuart Kemp <solpyro@gmail.com>
2025-01-14 12:01:39 +0200

    Add the Sincerify javascript bookmark snippet

---
+++ web/JavascriptSnippets/Sincerify.js
@@ -0,0 +1,2 @@
+///A Javascript snippet that can be saved as a bookmark, allowing you to set the font to Comic Sans on any webpage with a single mouse click
+javascript:(function(){a=["a","body","div","h1","h2","h3","h4","h5","h6","span","p","b","li","td","input","textarea","font","pre","select"];for(i=0;i<a.length;i++) {b=document.getElementsByTagName(a[i]);for(j=0;j<b.length;j++)b[j].style.fontFamily="'Comic Sans MS'";}})();
